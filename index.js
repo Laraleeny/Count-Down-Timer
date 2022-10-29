cb5eff5585207a3faed441e44ce97df2db4d61e2
Laraleeny <lkozlova1428@gmail.com>
2022-10-29 22:08:27 +0200

    Работа по пунктам задания 9.1

--- index.js
+++ index.js
@@ -1,21 +1,37 @@
 //создать переменные 
-let header = document.querySelector('#title-date');
-let date = document.querySelector('#date');
-let btnStart = document.querySelector('#btn');
-let btnReset = document.querySelector('#btn-reset');
+const header = document.querySelector('#title-date');
+const btnStart = document.querySelector('#btn');
+const btnReset = document.querySelector('#btn-reset');
 
 // сброс - все в начальное состояние
 const handleReset = () => {
     console.log('Reset');
-}
+};
 
 btnReset.addEventListener('click', handleReset);
 
-// при нажатии кнопки "Начать" запускается таймер
-const startTimer = () => {
-    console.log('D/H/M/S');
+const switchScreen = () => {
+    //скрыть кнопку "Начать" и вывести кнопку "Сброс"
+   btnStart.classList.add('hide');
+   btnReset.classList.remove('hide');
+   //скрыть блок с инпутами и вывести блок с аутпут
+   document.querySelector('.input').classList.add('hide');
+   document.querySelector('.output').classList.remove('hide');
 }
+
+// при нажатии кнопки "Начать" запускается таймер
+ const startTimer = () => {
+    //меняем значение заголовка
+    document.querySelector('h1').innerHTML = header.value;
+    header.value = '';
+   //проверка значения даты
+   const valueDate = document.querySelector('#date').value;
+   if (valueDate === '') {
+        alert("Пожалуйста введите дату");
+   } else {
+        switchScreen();
+   }
+};
 
 btnStart.addEventListener('click', startTimer);
 
-// комментарий чтобы содзать пул реквест
