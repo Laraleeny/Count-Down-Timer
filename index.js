85899593a806d6f046097e199828275d3c197efb
Laraleeny <lkozlova1428@gmail.com>
2022-10-30 14:07:09 +0200

    правки

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 //создать переменные 
 const header = document.querySelector('#title-date');
+const timerHeader = document.querySelector('h1');
 const btnStart = document.querySelector('#btn');
 const btnReset = document.querySelector('#btn-reset');
+const blockInput = document.querySelector('.input');
+const blockOutput = document.querySelector('.output');
+const dateMonth = document.querySelector('#date');
+let valueDate ;
 
 // сброс - все в начальное состояние
 const handleReset = () => {
@@ -15,17 +20,17 @@ const switchScreen = () => {
    btnStart.classList.add('hide');
    btnReset.classList.remove('hide');
    //скрыть блок с инпутами и вывести блок с аутпут
-   document.querySelector('.input').classList.add('hide');
-   document.querySelector('.output').classList.remove('hide');
+   blockInput.classList.add('hide');
+   blockOutput.classList.remove('hide');
 }
 
 // при нажатии кнопки "Начать" запускается таймер
  const startTimer = () => {
     //меняем значение заголовка
-    document.querySelector('h1').innerHTML = header.value;
+    timerHeader.innerHTML = header.value;
     header.value = '';
    //проверка значения даты
-   const valueDate = document.querySelector('#date').value;
+   valueDate = dateMonth.value;
    if (valueDate === '') {
         alert("Пожалуйста введите дату");
    } else {
