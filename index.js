67a636f580e397eee24185e99d3b91a1e6eeef28
Laraleeny <lkozlova1428@gmail.com>
2022-11-29 21:25:32 +0200

    Работа над комментами 11.2

--- index.js
+++ index.js
@@ -1,42 +1,44 @@
 //создать переменные
-const header = document.querySelector("#title-date");
-const timerHeader = document.querySelector("h1");
-const btnStart = document.querySelector("#btn");
-const btnReset = document.querySelector("#btn-reset");
-const blockInput = document.querySelector(".input");
-const blockOutput = document.querySelector(".output");
-const dateMonth = document.querySelector("#date");
+const header = document.querySelector('#title-date');
+const timerHeader = document.querySelector('h1');
+const btnStart = document.querySelector('#btn');
+const btnReset = document.querySelector('#btn-reset');
+const blockInput = document.querySelector('.input');
+const blockOutput = document.querySelector('.output');
+const dateMonth = document.querySelector('#date');
 let currentDate = moment(); // дата на сегодняшний день
-let timerDate;
+let timerDate, headerValue;
 let intervalID = null;
 
 timerDate = dateMonth.value;
-
-
+headerValue = header.value;
 // сброс - все в начальное состояние
 const handleReset = () => {
   timerHeader.textContent = 'Создать новый таймер обратного отсчета';
   header.value = '';
   dateMonth.valueAsDate = null;
+  document.querySelector('.complete').textContent = '';
+
   //скрыть кнопку "Сброс" и вывести кнопку "Начать"
-   btnStart.classList.remove("hide");
-   btnReset.classList.add("hide");
+   btnStart.classList.remove('hide');
+   btnReset.classList.add('hide');
    //скрыть блок с аутпут и вывести блок с инпутам
-   blockInput.classList.remove("hide");
-   blockOutput.classList.add("hide");
+   blockInput.classList.remove('hide');
+   blockOutput.classList.add('hide');
+
    localStorage.removeItem('header');
    localStorage.removeItem('date');
 };
 
-btnReset.addEventListener("click", handleReset);
+btnReset.addEventListener('click', handleReset);
 
 const switchScreen = () => {
   //скрыть кнопку "Начать" и вывести кнопку "Сброс"
-  btnStart.classList.add("hide");
-  btnReset.classList.remove("hide");
+  btnStart.classList.add('hide');
+  btnReset.classList.remove('hide');
   //скрыть блок с инпутами и вывести блок с аутпут
-  blockInput.classList.add("hide");
-  blockOutput.classList.remove("hide");
+  blockInput.classList.add('hide');
+  blockOutput.classList.remove('hide');
 };
 
 //создаем функцию для добовления "0" перед значением
@@ -51,59 +53,72 @@ const countDown = () => {
   //проверка на завершение таймера
   if (moment(timerDate).diff(currentDate) <= 0) {
     clearInterval(intervalID);
-    document.querySelector(".complete").classList.remove("hide");
+    document.querySelector('.complete').classList.remove('hide');
     document.querySelector(
-      ".complete"
-    ).textContent = `${header.value} завершился ${timerDate}`;
+      '.complete'
+    ).textContent = `${headerValue} завершился ${timerDate}`;
     return;
   }
-  const days = Math.floor(moment(timerDate).diff(currentDate, "days"));
-  const hours = Math.floor(moment(timerDate).diff(currentDate, "hours") % 24);
+  const days = Math.floor(moment(timerDate).diff(currentDate, 'days'));
+  const hours = Math.floor(moment(timerDate).diff(currentDate, 'hours') % 24);
   const minutes = Math.floor(
-    moment(timerDate).diff(currentDate, "minutes") % 60);
+    moment(timerDate).diff(currentDate, 'minutes') % 60);
   const seconds = Math.floor(
-    moment(timerDate).diff(currentDate, "seconds") % 60);
-  document.querySelector(".numbers").textContent = 
+    moment(timerDate).diff(currentDate, 'seconds') % 60);
+  document.querySelector('.numbers').textContent = 
   `${putZero(days)}:${putZero(hours)}:${putZero(minutes)}:${putZero(seconds)}`;
 };
+
+
+const startCount = () => {
+    switchScreen();
+    countDown();
+    //вызываем функцию каждую секунду
+    intervalID = setInterval(countDown, 1000);
+  
+}
 
 // при нажатии кнопки "Начать" запускается таймер
 const startTimer = () => {
   //меняем значение заголовка
-  timerHeader.innerHTML = header.value;
-  localStorage.setItem('header', header.value);
-
+  headerValue = header.value;
+  timerHeader.innerHTML = headerValue; 
   //дату отсчета запоминаем в переменную
   timerDate = dateMonth.value;
+  
+  localStorage.setItem('header', headerValue);
   localStorage.setItem('date', timerDate);
+  
   //проверка значения даты
   //проверка дата уже прошла
   if (moment(timerDate).isBefore(currentDate)) {
     alert("Дата уже прошла");
     return;
   }
-  if (timerDate === "") {
+  if (timerDate === '') {
     alert("Пожалуйста введите дату");
   } else {
-    switchScreen();
-    countDown();
-    //вызываем функцию каждую секунду
-    intervalID = setInterval(countDown, 1000);
+    startCount();
   }
+  
 };
 
 const isStorage = () => {
-  if (localStorage.getItem('header') !== '') {
+  if (!localStorage.getItem('header') 
+  && !localStorage.getItem('date')){
+    return;
+  }
+  if (!!localStorage.getItem('header') 
+  && !!localStorage.getItem('date')) {
     timerHeader.textContent = localStorage.getItem('header');
-  };
-  if (localStorage.getItem('date') !== '') {
     timerDate = localStorage.getItem('date');
   }
-  startTimer();
+  
+  startCount();
 }
 
 isStorage();
 
-btnStart.addEventListener("click", startTimer);
+btnStart.addEventListener('click', startTimer);
 
-// for commit
+
