7b3797694baa53d3eae3cce795bb160d21cdfb72
laraleeny <lkozlova1428@gmail.com>
2022-12-15 14:54:59 +0200

    работа над комментариями по классам

--- index.js
+++ index.js
@@ -1,130 +1,134 @@
 //создать переменные
-const header = document.querySelector('#title-date');
-const timerHeader = document.querySelector('h1');
-const btnStart = document.querySelector('#btn');
-const btnReset = document.querySelector('#btn-reset');
-const blockInput = document.querySelector('.input');
-const blockOutput = document.querySelector('.output');
-const dateMonth = document.querySelector('#date');
-const headerComplete = document.querySelector('.complete');
-// let currentDate = moment(); // дата на сегодняшний день
-// let timerDate;
+const header = document.querySelector("#title-date");
+const timerHeader = document.querySelector("h1");
+const btnStart = document.querySelector("#btn");
+const btnReset = document.querySelector("#btn-reset");
+const blockInput = document.querySelector(".input");
+const blockOutput = document.querySelector(".output");
+const dateMonth = document.querySelector("#date");
+const headerComplete = document.querySelector(".complete");
 let headerValue;
-let intervalID = null;
+let timer;
 
 class Timer {
   constructor(date) {
     this.timerDate = date;
     this.currentDate = null;
+    this.intervalID = null;
   }
+
+  // setDate(){
+  //   this.timerDate = dateMonth.value;
+  // }
 
   //создаем функцию для добовления "0" перед значением
   putZero(dateID) {
-    return dateID < 10 ? "0" + dateID : dateID;
-  }
+    let result = dateID < 10 ? "0" + dateID : dateID;
+    return result;
+  };
 
   //создаем функцию с обратным отсчетом
   countDown() {
     this.currentDate = moment();
-  //проверка на завершение таймера
-    if (moment(this.timerDate).diff(this.currentDate) <= 0) {
-    clearInterval(intervalID);
-    headerComplete.classList.remove('hide');
-    headerComplete.textContent = `${headerValue} завершился ${this.timerDate}`;
-    return;
-    }
-    const days = Math.floor(moment(this.timerDate).diff(this.currentDate, 'days'));
-    const hours = Math.floor(moment(this.timerDate).diff(this.currentDate, 'hours') % 24);
-    const minutes = Math.floor(
-    moment(this.timerDate).diff(this.currentDate, 'minutes') % 60);
-    const seconds = Math.floor(
-    moment(this.timerDate).diff(this.currentDate, 'seconds') % 60);
-    document.querySelector('.numbers').textContent = 
-    `${timer.putZero(days)}:${timer.putZero(hours)}:${timer.putZero(minutes)}:${timer.putZero(seconds)}`;
-    };
-}
 
-let timer = new Timer(dateMonth.value);
+    //проверка на завершение таймера
+    if (moment(this.timerDate).diff(this.currentDate) <= 0) {
+      clearInterval(this.intervalID);
+      headerComplete.classList.remove("hide");
+      headerComplete.textContent = `${headerValue} завершился ${this.timerDate}`;
+      return;
+    }
+    const days = Math.floor(moment(this.timerDate).diff(this.currentDate, "days"));
+    const hours = Math.floor(moment(this.timerDate).diff(this.currentDate, "hours") % 24);
+    const minutes = Math.floor(moment(this.timerDate).diff(this.currentDate, "minutes") % 60);
+    const seconds = Math.floor(moment(this.timerDate).diff(this.currentDate, "seconds") % 60);
+    document.querySelector(".numbers").textContent = `${this.putZero(days)}:${this.putZero(hours)}:${this.putZero(
+      minutes
+    )}:${this.putZero(seconds)}`;
+  }
+
+  init() {
+    this.countDown();
+    //вызываем функцию каждую секунду
+    this.intervalID = setInterval(this.countDown.bind(this), 1000);
+  }
+
+  //проверка значения даты
+  //проверка дата уже прошла
+  checkDate() {
+    this.currentDate = moment();
+    if (moment(this.timerDate).isBefore(this.currentDate)) {
+      alert("Дата уже прошла");
+      return;
+    }
+    if (this.timerDate === "") {
+      alert("Пожалуйста введите дату");
+      return;
+    } else {
+      startCount();
+    }
+  }
+}
 
 // сброс - все в начальное состояние
 const handleReset = () => {
-  clearInterval(intervalID);
-  timerHeader.textContent = 'Создать новый таймер обратного отсчета';
-  header.value = '';
+  clearInterval(timer.intervalID);
+  timerHeader.textContent = "Создать новый таймер обратного отсчета";
+  header.value = "";
   dateMonth.valueAsDate = null;
-  headerComplete.textContent = '';
+  headerComplete.textContent = "";
 
   //скрыть кнопку "Сброс" и вывести кнопку "Начать"
-   btnStart.classList.remove('hide');
-   btnReset.classList.add('hide');
-   //скрыть блок с аутпут и вывести блок с инпутам
-   blockInput.classList.remove('hide');
-   blockOutput.classList.add('hide');
+  btnStart.classList.remove("hide");
+  btnReset.classList.add("hide");
+  //скрыть блок с аутпут и вывести блок с инпутам
+  blockInput.classList.remove("hide");
+  blockOutput.classList.add("hide");
 
-   localStorage.removeItem('header');
-   localStorage.removeItem('date');
+  localStorage.removeItem("header");
+  localStorage.removeItem("date");
 };
 
-btnReset.addEventListener('click', handleReset);
+btnReset.addEventListener("click", handleReset);
 
 const switchScreen = () => {
   //скрыть кнопку "Начать" и вывести кнопку "Сброс"
-  btnStart.classList.add('hide');
-  btnReset.classList.remove('hide');
+  btnStart.classList.add("hide");
+  btnReset.classList.remove("hide");
   //скрыть блок с инпутами и вывести блок с аутпут
-  blockInput.classList.add('hide');
-  blockOutput.classList.remove('hide');
+  blockInput.classList.add("hide");
+  blockOutput.classList.remove("hide");
 };
 
-const startCount = () => {
-    switchScreen();
-    timer.countDown();
-    //вызываем функцию каждую секунду
-    intervalID = setInterval(timer.countDown, 1000);
-  
-}
+const startCount = (localData) => {
+  switchScreen();
+  localData? timer = new Timer(localData) :  timer = new Timer(dateMonth.value);
+  timer.init();
+};
 
 // при нажатии кнопки "Начать" запускается таймер
 const startTimer = () => {
+  timer = new Timer(dateMonth.value);
+  timer.checkDate();
   //меняем значение заголовка
   headerValue = header.value;
-  timerHeader.innerHTML = headerValue; 
-  //дату отсчета запоминаем в переменную
-  this.timerDate = dateMonth.value;
-  
-  localStorage.setItem('header', headerValue);
-  localStorage.setItem('date', this.timerDate);
-  
-  //проверка значения даты
-  //проверка дата уже прошла
-  if (moment(this.timerDate).isBefore(this.currentDate)) {
-    alert("Дата уже прошла");
-    return;
-  }
-  if (this.timerDate === '') {
-    alert("Пожалуйста введите дату");
-  } else {
-    startCount();
-  }
-  
+  if(!!headerValue)
+  {timerHeader.innerHTML = headerValue;}
+
+  localStorage.setItem("header", headerValue);
+  localStorage.setItem("date", timer.timerDate);
 };
 
 const isStorage = () => {
-  const headerStorage = localStorage.getItem('header');
-  const dateStorage = localStorage.getItem('date');
-  if (!headerStorage && !dateStorage){
+  const headerStorage = localStorage.getItem("header");
+  const dateStorage = localStorage.getItem("date");
+  if (!headerStorage && !dateStorage) {
     return;
   }
-  if (!!headerStorage) {
-    timerHeader.textContent = headerStorage;
-    this.timerDate = dateStorage;
-  }
-  
-  startCount();
-}
+
+  startCount(dateStorage);
+};
 
 isStorage();
 
-btnStart.addEventListener('click', startTimer);
-
-
+btnStart.addEventListener("click", startTimer);
